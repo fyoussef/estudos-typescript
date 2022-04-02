caffe2da0e86eaad1bdb0fd8c83ed156b228aa79
fyoussef <fillipyuou@gmail.com>
2022-04-02 20:33:21 -0300

    :books: Aula sobre o tipo ANY -> Qualquer coisa

---
+++ src/aula2-tipo-any/index.ts
@@ -0,0 +1,11 @@
+// Ultilize any como último recurso
+/* function showMessage(msg: any) {
+    return msg
+} */
+
+function showMessage(msg: string) {
+    return msg
+}
+
+console.log(showMessage('QUALQUER COISA QUANDO É TIPADO COM ANY'))
+console.log(['QUALQUER', 'COISA'])
